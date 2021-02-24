ae016db2008f32c660cb67e6561277c8fb81474e
omeruzunogullari <79421453+omeruzunogullari@users.noreply.github.com>
2021-02-25 00:23:31 +0300

    OdevNo:1

---
+++ Untitled.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+anapara=1000
+gunsayisi= input("Gün sayısı giriniz:")
+sonpara = (anapara + (anapara* (0.12))) * float(gunsayisi)
+print(sonpara)
+
+
+# In[ ]:
+
+
+x=1000
+y=12
+z=1210.68
+print("Hafta başında" + " "  +  str(x) + " " + "dolarlık bitcoin aldığımızda günde ortalama %" + str(y) + " " + "kazançla, bir hafta sonunda" +" " + str(z) + " "+ "dolar kazanırdık", sep=" ")
+
+
+# In[ ]:
+
+
+dosya_adi = input("Dosya adını giriniz")
+print("Dosyanının adı: {}.py".format(dosya_adi))
+
+
+# In[ ]:
+
+
+
+
